80f050446710a8ceeb29ee91068852ee8e3adc7f
Ramadhani Musthofa <ramadhani.musthofa@gdn-commerce.com>
2023-07-25 15:15:12 +0700

    create order endpoint

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,19 +1,17 @@
 package com.example.plugins
 
 import com.example.routes.customerRouting
-import io.ktor.serialization.kotlinx.json.*
+import com.example.routes.getOrderRoute
+import com.example.routes.listOrdersRoute
+import com.example.routes.totalizeOrderRoute
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         customerRouting()
-    }
-}
-
-fun Application.configureSerialization() {
-    install(ContentNegotiation) {
-        json()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
